507c5fd8dace7f260f91da6766f70e442f2932e1
3123544976 <3123544976@qq.com>
2025-08-04 11:24:08 +0800

    时间选择器使用中文

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import './style.css'
 // import App from './TaskManagementSystem.vue'
 import App from './App.vue' 
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import 'element-plus/theme-chalk/index.css'
 import axios from 'axios'
 import router from './router'
 
 const app = createApp(App)
 app.use(router)
-app.use(ElementPlus)
+app.use(ElementPlus, { locale: zhCn })
 app.config.globalProperties.$axios = axios
 app.mount('#app')
