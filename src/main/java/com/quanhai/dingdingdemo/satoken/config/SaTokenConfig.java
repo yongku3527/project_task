53ce33e68739ee7838d930aa4cb6109a84a463c4
3123544976 <3123544976@qq.com>
2025-10-24 11:03:11 +0800

    1.51 只拦截需要拦截的接口

--- src/main/java/com/quanhai/dingdingdemo/satoken/config/SaTokenConfig.java
+++ src/main/java/com/quanhai/dingdingdemo/satoken/config/SaTokenConfig.java
@@ -23,27 +23,46 @@ public class SaTokenConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         // 注册Sa-Token拦截器，打开注解式鉴权功能
         registry.addInterceptor(new SaInterceptor(handle -> {
-            // 指定一条 match 规则
-            SaRouter
-                    .match("/**")    // 拦截的 path 列表，可以写多个
-                    .notMatch("/auth/login")    // 排除掉的 path 列表，可以写多个
-                    .notMatch("/auth/register")
-                    //临时排除权限接口
-                    .notMatch("/auth/permission")
-                    .notMatch("/auth/role")
-                    .notMatch("/test/cookie")    // 排除cookie测试接口
+//            // 指定一条 match 规则
+//            SaRouter
+//                    .match("/**")    // 拦截的 path 列表，可以写多个
+//                    .notMatch("/auth/login")    // 排除掉的 path 列表，可以写多个
+//                    .notMatch("/auth/register")
+//                    //临时排除权限接口
+//                    .notMatch("/auth/permission")
+//                    .notMatch("/auth/role")
+//                    .notMatch("/test/cookie")    // 排除cookie测试接口
+//
+//                    // 放行游客访问
+//                    .notMatch("/dingTask/**")
+//                    .notMatch("/TimeLine/**")
+//                    .notMatch("/meeting/**")
+//                    .notMatch("/basicData/**")
+//                    .notMatch("/gantt/**")
+//                    .notMatch("/file-manager/**")
+//                    .notMatch("/file-info/**")
+//                    .notMatch("/minio/**")
+//
+//                    .notMatch("/error")
+//                    .notMatch("/swagger-ui/**")
+//                    .notMatch("/swagger-resources/**")
+//                    .notMatch("/webjars/**")
+//                    .notMatch("/v2/api-docs")
+//                    .notMatch("/v3/api-docs")
+//                    .notMatch("/")    // 排除首页
+//                    .notMatch("/index.html")    // 排除首页
+//                    .notMatch("/crud-test.html")    // 排除CRUD测试页面
+//                    .notMatch("/static/**")    // 排除静态资源
+//                    .check(r -> StpUtil.checkLogin());        // 要执行的校验动作，可以写完整的 lambda 表达式
 
-                    .notMatch("/error")
-                    .notMatch("/swagger-ui/**")
-                    .notMatch("/swagger-resources/**")
-                    .notMatch("/webjars/**")
-                    .notMatch("/v2/api-docs")
-                    .notMatch("/v3/api-docs")
-                    .notMatch("/")    // 排除首页
-                    .notMatch("/index.html")    // 排除首页
-                    .notMatch("/crud-test.html")    // 排除CRUD测试页面
-                    .notMatch("/static/**")    // 排除静态资源
-                    .check(r -> StpUtil.checkLogin());        // 要执行的校验动作，可以写完整的 lambda 表达式
+
+            SaRouter.match("/sys/**"
+                            ,"/circuit-board/**"
+                            ,"/semi-product/**"
+                            ,"/meeting/**")
+                    .check(r -> StpUtil.checkLogin());
+
+
         })).addPathPatterns("/**");
     }
 
