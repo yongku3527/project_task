762e029ebb7a3528cb0df1522da5d1e251f0c708
3123544976 <3123544976@qq.com>
2025-12-02 08:45:58 +0800

    1.7.49 删除经验库条目同时删除minio的文件，创建规格书实体类

---
+++ src/main/java/com/quanhai/dingdingdemo/file/model/Specification.java
@@ -0,0 +1,79 @@
+package com.quanhai.dingdingdemo.file.model;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@TableName("doc_specification")
+public class Specification {
+
+
+    /**
+     * 主键ID
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+
+    /**
+     * 原材料id
+     */
+    @TableField("material_id")
+    private String materialId;
+
+    /**
+     * 文件类别
+     */
+    @TableField("drawing_type")
+    private String drawingType;
+
+    /**
+     * 物料名称
+     */
+    @TableField("item_name")
+    private String itemName;
+
+    /**
+     * 规格型号
+     */
+    @TableField("model")
+    private String model;
+
+    /**
+     * 文件ID
+     */
+    @TableField("file_id")
+    private Long fileId;
+
+
+    /**
+     * 文件URL
+     */
+    @TableField(exist = false)
+    private String fileUrl;
+
+    /**
+     * 文件名称
+     */
+    @TableField(exist = false)
+    private String fileName;
+
+    /**
+     * 状态（0：停用，1：启用）
+     */
+    private Integer status;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+}
